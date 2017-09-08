19023a3db6a2f495e6a9da985a3a5e8eb86ddf3b
JunWoong Cha <chawnsnd@gmail.com>
2017-09-08 13:04:09 +0900

    주석제거

--- src/jdbc/ConnectionProvider.java
+++ src/jdbc/ConnectionProvider.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 
 public class ConnectionProvider {
 	public static Connection getConnection() throws Exception{
-		//url �� db���� �ּ�
 		String url = "";
 		return DriverManager.getConnection(url);
 	}
--- src/member/LogonDBBean.java
+++ src/member/LogonDBBean.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import jdbc.ConnectionProvider;
 
 public class LogonDBBean {
-	// 싱글턴 패턴 <== 쓰레드로 여러사람이 이 빈의 객체에 동시 접근 하는것을 방지하기 위해 사용
 	private static LogonDBBean instance = new LogonDBBean();
 
 	public static LogonDBBean getInstance() {
@@ -19,7 +18,6 @@ public class LogonDBBean {
 	}
 
 	public void insertMember(LogonDataBean member) throws Exception {
-		// sql 쿼리 삽입 테이블 칼럼이 몇 개 필요한지 몰라서 작성 안함
 		String sql = "insert into MEMBER values(?,?,?,?,?,?,?,?,?)";
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -28,12 +26,10 @@ public class LogonDBBean {
 			pstmt = conn.prepareStatement(sql);
 
 			setAllValues(pstmt, member);
-			// 맴버 정보 선언
 			pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			// 연결 닫아줌
 			if (pstmt != null)
 				try {
 					pstmt.close();
@@ -47,7 +43,6 @@ public class LogonDBBean {
 		}
 	}
 	public void updateMember(LogonDataBean member) throws Exception{
-		//수정 필요
 		String sql = "update MEMBER set ";
 		
 		Connection conn = null;
@@ -57,12 +52,10 @@ public class LogonDBBean {
 			pstmt = conn.prepareStatement(sql);
 
 			setAllValues(pstmt, member);
-			// 맴버 정보 선언
 			pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			// 연결 닫아줌
 			if (pstmt != null)
 				try {
 					pstmt.close();
@@ -78,13 +71,11 @@ public class LogonDBBean {
 		
 	}
 	public int userCheck(String id, String passwd) throws Exception{
-		//sql 문
 		String sql = "select passwd from member where id=?";
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String dbpasswd ="";
-		//x는 인증관련 숫자
 		int x = -1;
 		try {
 			conn = ConnectionProvider.getConnection();
@@ -96,11 +87,11 @@ public class LogonDBBean {
 			if(rs.next()) {
 				dbpasswd = rs.getString("passwd");
 				if(dbpasswd.equals(passwd))
-					x=1;		//인증 성공
+					x=1;		
 				else
-					x=0;		//인증 실패
+					x=0;		
 			}else
-				x=-1;			//해당 아이디 없다
+				x=-1;			
 		}catch(Exception ex) {
 			ex.printStackTrace();
 		}finally {
@@ -115,7 +106,6 @@ public class LogonDBBean {
 	}
 	public void setAllValues(PreparedStatement pstmt, LogonDataBean member) {
 		try {
-			//입력만
 			pstmt.setString(1, member.getId());
 			pstmt.setString(2, member.getPasswd());
 			pstmt.setString(3, member.getName());
