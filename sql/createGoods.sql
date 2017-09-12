79f389f864b5251ce0db6cba4dab9af9983c5f69
JunWoong Cha <chawnsnd@gmail.com>
2017-09-12 20:57:15 +0900

    sql 사용을 위한 수정

--- sql/createGoods.sql
+++
@@ -1,8 +0,0 @@
-create table goods(
-	no int not null primary key,
-    name varchar(20) not null,
-    price int,
-    company varchar(20),
-    stock int,
-    category varchar(20)
-)engine=InnoDB default character set = utf8;
